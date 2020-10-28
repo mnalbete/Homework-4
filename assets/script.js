ff71860ffcbd93a2e42a5c94e633d67501ce04e8
mnalbete <mnalbete@uw.edu>
2020-10-27 23:12:27 -0700

    stuff

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,54 @@
 // need a timer on top right to show the time the quiz ends
 // timer starts at when you press the button on the quiz
 
-var myVar = setInterval(myTimer, 1000);
-var timerSeconds = 100;
-function myTimer() {
-    timerSeconds--;
-    document.getElementById("timer").innerHTML = timerSeconds;
-}
-var questionObj = {
-    question: "this is my first question",
-    answerOne: "this is the first answer",
-    answerTwo: "this is the second answer",
-    answerThree: "this is the third answer",
-}
 //start button if clicked display first question
-document.getElementById("startButton").addEventListener("click",function(e){
+
+
+
+
+
+document.getElementById("startButton").addEventListener("click", function (e) {
+    var myVar = setInterval(myTimer, 1000);
+    var timerSeconds = 100;
+    function myTimer() {
+        timerSeconds--;
+        document.getElementById("timer").innerHTML = timerSeconds;
+    }
+    var questionObj = {
+        question: "this is my first question",
+        answerOne: "this is the first answer",
+        answerTwo: "this is the second answer",
+        answerThree: "this is the third answer",
+        answerFour: "this is the forth answer",
+    }
+    var questionObj = {
+        question: "this is my first question",
+        answerOne: "this is the first answer",
+        answerTwo: "this is the second answer",
+        answerThree: "this is the third answer",
+        answerFour: "this is the forth answer",
+    }
+    var questionObj = {
+        question: "this is my first question",
+        answerOne: "this is the first answer",
+        answerTwo: "this is the second answer",
+        answerThree: "this is the third answer",
+        answerFour: "this is the forth answer",
+    }
+
+
     console.log(questionObj.question)
-    console.log(e.target.textContent) 
+    console.log(e.target.textContent)
     // display the first question and the answer choices
     document.getElementById("questionText").innerHTML = questionObj.question;
     document.getElementById("answerOne").innerHTML = questionObj.answerOne;
-    document.getElementById("answerTwo").innerHTML = questionObj.answerOne; 
-    })
+    document.getElementById("answerTwo").innerHTML = questionObj.answerTwo;
+    document.getElementById("answerThree").innerHTML = questionObj.answerThree;
+    document.getElementById("answerFour").innerHTML = questionObj.answerFour;
+})
+
+
+
 
 
 //need a user select from user choices 
